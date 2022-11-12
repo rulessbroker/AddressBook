7e38284eb50367ff0c731ebf68af1b43c76ad23e
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-12 13:54:59 +0530

    Added: Ability to add existing contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -49,4 +49,58 @@ public class AddressBook {
 		System.out.println("person created sucessfully");
 	}
 
+	public void listPeople() {
+		if (people.isEmpty()) {
+			System.out.println("Your address book is empty.");
+			return;
+		}
+		System.out.println("\n Address Book:");
+		for (Contact person : people) {
+			System.out.println(person.getFirstName());
+		}
+
+	}
+
+	public void editPerson() {
+		System.out.println("Enter the first name you want to edit.");
+
+		String firstName = sc.nextLine();
+
+		for (int i = 0; i < people.size(); i++) {
+			if (people.get(i).getFirstName().equals(firstName)) {
+
+				people.get(i).setFirstName(firstName);
+
+				System.out.println("Enter Last Name:");
+				String lastName = sc.nextLine();
+				people.get(i).setLastName(lastName);
+
+				System.out.println("Enter Adress:");
+				String address = sc.nextLine();
+				people.get(i).setAddress(address);
+
+				System.out.println("Enter state:");
+				String state = sc.nextLine();
+				people.get(i).setState(state);
+
+				System.out.println("Enter zip:");
+				int zip = sc.nextInt();
+				people.get(i).setZip(zip);
+
+				System.out.println("Enter phoneNumber:");
+				long phoneNumber = sc.nextLong();
+				people.get(i).setPhoneNumber(phoneNumber);
+
+				System.out.println("Enter email:");
+				String email = sc.nextLine();
+				people.get(i).setEmail(email);
+
+				System.out.println("person edited sucessfully");
+			} else {
+				System.out.println("person not found");
+			}
+
+		}
+	}
+
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,12 +1,36 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
 
-		System.out.println("***Welcome To Address Book Program***\n\n");
+		System.out.println("\n***Welcome To Address Book Program***\n");
 
+		Scanner input = new Scanner(System.in);
 		AddressBook details = new AddressBook();
-		details.addContacts();
 
+		int choice;
+		do {
+			System.out.println("\n1.Add contact\n2.Show Contact\n3.Edit Contact\n4.Exit\n");
+			System.out.println("Enter your choice: ");
+			choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				details.addContacts();
+				break;
+			case 2:
+				details.listPeople();
+				break;
+			case 3:
+				details.editPerson();
+				break;
+			case 4:
+				System.out.println("Program Terminate Successfully!!!");
+				break;
+			default:
+				System.out.println("Enter Valid Input...");
+			}
+		} while (choice != 4);
 	}
 }
