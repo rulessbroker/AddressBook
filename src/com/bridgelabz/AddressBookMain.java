70156aa609f9d2bcde37bed8bef71b6dad656e7f
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-17 10:12:12 +0530

    Added: Add Multiple Persons

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -12,28 +12,29 @@ public class AddressBookMain {
 		AddressBook details = new AddressBook();
 
 		int choice;
-
-		System.out.println("\n1.Add contact\n2.Show Contact\n3.Edit Contact\n4.Delete Contact\n5.Exit\n");
-		System.out.println("Enter your choice: ");
-		choice = input.nextInt();
-		switch (choice) {
-		case 1:
-			details.addContacts();
-			break;
-		case 2:
-			details.listPeople();
-			break;
-		case 3:
-			details.editPerson();
-			break;
-		case 4:
-			details.removeContact();
-			break;
-		case 5:
-			System.out.println("Program Terminate Successfully!!!");
-			break;
-		default:
-			System.out.println("Enter Valid Input...");
-		}
+		do {
+			System.out.println("\n1.Add contact\n2.Show Contact\n3.Edit Contact\n4.Delete Contact\n5.Exit\n");
+			System.out.println("Enter your choice: ");
+			choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				details.addContacts();
+				break;
+			case 2:
+				details.listPeople();
+				break;
+			case 3:
+				details.editPerson();
+				break;
+			case 4:
+				details.removeContact();
+				break;
+			case 5:
+				System.out.println("Program Terminate Successfully!!!");
+				break;
+			default:
+				System.out.println("Enter Valid Input...");
+			}
+		} while (choice != 5);
 	}
 }
