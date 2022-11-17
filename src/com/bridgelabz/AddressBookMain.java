2a5fcc490183594e8d2bff6d999ee6f004229008
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-17 09:55:33 +0530

    Update: ammend the data

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -11,26 +11,25 @@ public class AddressBookMain {
 		AddressBook details = new AddressBook();
 
 		int choice;
-		do {
-			System.out.println("\n1.Add contact\n2.Show Contact\n3.Edit Contact\n4.Exit\n");
-			System.out.println("Enter your choice: ");
-			choice = input.nextInt();
-			switch (choice) {
-			case 1:
-				details.addContacts();
-				break;
-			case 2:
-				details.listPeople();
-				break;
-			case 3:
-				details.editPerson();
-				break;
-			case 4:
-				System.out.println("Program Terminate Successfully!!!");
-				break;
-			default:
-				System.out.println("Enter Valid Input...");
-			}
-		} while (choice != 4);
+
+		System.out.println("\n1.Add contact\n2.Show Contact\n3.Edit Contact\n4.Exit\n");
+		System.out.println("Enter your choice: ");
+		choice = input.nextInt();
+		switch (choice) {
+		case 1:
+			details.addContacts();
+			break;
+		case 2:
+			details.listPeople();
+			break;
+		case 3:
+			details.editPerson();
+			break;
+		case 4:
+			System.out.println("Program Terminate Successfully!!!");
+			break;
+		default:
+			System.out.println("Enter Valid Input...");
+		}
 	}
 }
